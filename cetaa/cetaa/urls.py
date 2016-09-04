ceaad4ee74d5414570d5e05f706af1573259cf07
26shame <26shame@gmail.com>
2016-09-04 12:03:48 +0530

    Uninstalled Django-registration

--- cetaa/cetaa/urls.py
+++ cetaa/cetaa/urls.py
@@ -11,13 +11,6 @@ urlpatterns = [
 	url(r'^$', index),
     # About views
 	url(r'^about', about_index),
-    # Accounts views
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
-    url(r'^accounts/register/$', register, name='register'),
-    url(r'^accounts/register/complete/$', registration_complete, name='registration_complete'),
-    url(r'^accounts/login/$', login, name='login'),
-    url(r'^accounts/logout/$', logout, name='logout'),
-    url(r'^accounts/loggedin/$', loggedin, name='loggedin'),
     # Admin views
     url(r'^admin/', admin.site.urls),
 ]
