048f676a2c3f17d6ff01befba59f6578559ae217
26shame <26shame@gmail.com>
2016-09-04 12:03:48 +0530

    Fixed urls

--- README.md
+++
@@ -1,1 +0,0 @@
-# cetaaconnect1
--- cetaa/cetaa/urls.py
+++ cetaa/cetaa/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import index
 from about1.views import about_index
-from cetaa.views import register, registration_complete, loggedin
-from django.contrib.auth.views import login, logout
 
 
 urlpatterns = [
@@ -11,13 +9,6 @@ urlpatterns = [
 	url(r'^$', index),
     # About views
 	url(r'^about', about_index),
-    # Accounts views
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
-    url(r'^accounts/register/$', register, name='register'),
-    url(r'^accounts/register/complete/$', registration_complete, name='registration_complete'),
-    url(r'^accounts/login/$', login, name='login'),
-    url(r'^accounts/logout/$', logout, name='logout'),
-    url(r'^accounts/loggedin/$', loggedin, name='loggedin'),
     # Admin views
     url(r'^admin/', admin.site.urls),
 ]
